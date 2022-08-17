a650a915362789931b647b60a1543cd2eb57bd4e
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-17 07:28:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0, 2);
+};
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(num) {
+  if (num === 2) {
+    return fareDoubler;
+  } else if (num === 3) {
+    return fareTripler;
+  } else if (num === 5) {
+    return fareQuintupler;
+  }
+}
+function fareDoubler(fare) {
+  return fare * 2;
+}
+function fareTripler(fare) {
+  return fare * 3;
+}
+function fareQuintupler(fare) {
+  return fare * 5;
+}
+function selectDifferentDrivers(arr, driver) {
+  if (driver === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arr);
+  } else if (driver === returnLastTwoDrivers) {
+    return returnLastTwoDrivers(arr);
+  }
+}
